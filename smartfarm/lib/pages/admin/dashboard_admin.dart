abf615a35dba936553826c87e516c709a0ebc4e2
malikibrahim10203 <malikibr4h1@gmail.com>
2023-11-04 22:40:53 +0700

    prototype dashboard

--- smartfarm/lib/pages/admin/dashboard_admin.dart
+++ smartfarm/lib/pages/admin/dashboard_admin.dart
@@ -13,13 +13,147 @@ class _DashboardAdminState extends State<DashboardAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: OutlinedButton(
-          onPressed: () {
-            EventPref.clear();
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-          },
-          child: Text('LogOut'),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 262,
+            child: Container(
+              color: Color(0xff5369FE),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "SmartFarm",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15
+                        ),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          EventPref.clear();
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+                        },
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        child: Text('LogOut', style: TextStyle(color: Colors.white),),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: (){},
+                            icon: Icon(
+                              Icons.map,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Total Lahan",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "2",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: (){},
+                            icon: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Total Pengguna",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "2",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          IconButton(
+                            onPressed: (){},
+                            icon: Icon(
+                              Icons.devices,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Total Device",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "2",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
