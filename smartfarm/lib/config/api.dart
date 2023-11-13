5f4c3a405095d84509a814f38188171a690b08b6
harief285 <harief285@gmail.com>
2023-11-13 20:06:21 +0700

    tampilan user(farmer)

--- smartfarm/lib/config/api.dart
+++ smartfarm/lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const host = 'http://192.168.138.51/api_smartfarm';
+  static const host = 'http://192.168.1.142/API_smartfarm';
   static String login = '$host/login.php';
   static String list_user = '$host/get_user.php';
 }
--- smartfarm/lib/pages/farmer/dashboard_farmer.dart
+++ smartfarm/lib/pages/farmer/dashboard_farmer.dart
@@ -13,14 +13,217 @@ class DashboardFarmer extends StatefulWidget {
 class _DashboardFarmerState extends State<DashboardFarmer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: OutlinedButton(
-          onPressed: () {
-            EventPref.clear();
-            Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
-          },
-          child: Text('LogOut'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.white, Color(0xffC7EEFF)],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(left: 30, top: 50, right: 30),
+                child: Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.account_circle_rounded,
+                            size: 25,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              EventPref.clear();
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
+                            },
+                            icon: Icon(
+                                Icons.logout
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Hi, User')
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    SizedBox(
+                      height: 150,
+                      width: 420,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: BorderSide(
+                            width: 1,
+                            color: Color(0xffD5D5D5),
+                          ),
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(25),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "16°C",
+                                    style: TextStyle(
+                                      fontSize: 25,
+                                      color: Color(0xff505050),
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.wb_cloudy_outlined,
+                                    size: 30,
+                                    color: Color(0xff505050),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Cuaca",
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          child: SizedBox(
+                            height: 150,
+                            width: 170,
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                side: BorderSide(
+                                  width: 1,
+                                  color: Color(0xffD5D5D5),
+                                ),
+                              ),
+                              child: Container(
+                                padding: EdgeInsets.all(25),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(
+                                          Icons.map_outlined,
+                                          size: 30,
+                                          color: Color(0xff505050),
+                                        ),
+                                        Text(
+                                          "2",
+                                          style: TextStyle(
+                                              fontSize: 25
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 35,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Total Lahan",
+
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 150,
+                          width: 170,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              side: BorderSide(
+                                width: 1,
+                                color: Color(0xffD5D5D5),
+                              ),
+                            ),
+                            child: Container(
+                              padding: EdgeInsets.all(25),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Icon(
+                                        Icons.device_hub_outlined,
+                                        size: 30,
+                                        color: Color(0xff505050),
+                                      ),
+                                      Text(
+                                        "2",
+                                        style: TextStyle(
+                                            fontSize: 25
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 35,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Total Device",
+
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
