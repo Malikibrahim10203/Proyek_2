c59255a9c313b41d4e7d79fe4ca4d54a8e5702c8
harief285 <harief285@gmail.com>
2023-12-13 16:46:44 +0700

    fix api cloud

--- smartfarm/lib/config/api.dart
+++ smartfarm/lib/config/api.dart
@@ -1,6 +1,6 @@
 class Api {
 
-  static const host = 'http://192.168.109.51/api_smartfarm';
+  static const host = 'https://smartfarm.rafliseptiannn25.web.ti.polindra.ac.id/';
 
   static String login = '$host/login.php';
   static String list_user = '$host/get_user.php';
