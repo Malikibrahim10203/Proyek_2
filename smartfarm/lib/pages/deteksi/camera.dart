e80a5737d319cbc61a47b8adf5be9c43848c833d
harief285 <harief285@gmail.com>
2023-12-15 21:12:12 +0700

    add camera api+preview (native)

--- smartfarm/lib/pages/deteksi/camera.dart
+++ smartfarm/lib/pages/deteksi/camera.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:smartfarm/model/response_model.dart';
 import 'package:smartfarm/pages/deteksi/preview.dart';
 
 class CameraPage extends StatefulWidget {
@@ -9,12 +13,93 @@ class CameraPage extends StatefulWidget {
   final List<CameraDescription>? cameras;
 
   @override
-  State<CameraPage> createState() => _CameraPageState();
+  _CameraPageState createState() => _CameraPageState();
 }
 
 class _CameraPageState extends State<CameraPage> {
   late CameraController _cameraController;
-  bool _isRearCameraSelected = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  void _initCamera() async {
+    _cameraController =
+        CameraController(widget.cameras![0], ResolutionPreset.high);
+    await _cameraController.initialize();
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _takePicture() async {
+  try {
+    if (!_cameraController.value.isInitialized) {
+      return;
+    }
+    XFile picture = await _cameraController.takePicture();
+    ApiResponse apiResponse = await _uploadPicture(picture);
+    
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PreviewPage(
+          picture: picture,
+          apiResponse: apiResponse,
+        ),
+      ),
+    );
+  } catch (e) {
+    print('Error taking picture: $e');
+  }
+}
+
+  Future<ApiResponse> _uploadPicture(XFile picture) async {
+  try {
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://103.149.71.213:8003/prediction'),
+    );
+
+    request.files.add(await http.MultipartFile.fromPath('file', picture.path));
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData =
+          json.decode(await response.stream.bytesToString());
+          
+      return ApiResponse.fromJson(responseData['data']);
+    } else {
+      print('Failed to upload picture. Status code: ${response.statusCode}');
+      // Handle the error case if needed
+      return ApiResponse(
+        prediction: '',
+        confidence: 0.0,
+        imagePath: '',
+        fileName: '',
+        description: '',
+        pengendalianHayati: '',
+        pengendalianKimiawi: '',
+      );
+    }
+  } catch (e) {
+    print('Error uploading picture: $e');
+    // Handle the error case if needed
+    return ApiResponse(
+      prediction: '',
+      confidence: 0.0,
+      imagePath: '',
+      fileName: '',
+      description: '',
+      pengendalianHayati: '',
+      pengendalianKimiawi: '',
+    );
+  }
+}
+
 
   @override
   void dispose() {
@@ -23,94 +108,18 @@ class _CameraPageState extends State<CameraPage> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    initCamera(widget.cameras![0]);
-  }
-
-  Future takePicture() async {
-    if (!_cameraController.value.isInitialized) {
-      return null;
-    }
-    if (_cameraController.value.isTakingPicture) {
-      return null;
-    }
-    try {
-      await _cameraController.setFlashMode(FlashMode.off);
-      XFile picture = await _cameraController.takePicture();
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => PreviewPage(
-                picture: picture,
-              )));
-    } on CameraException catch (e) {
-      debugPrint('Error occured while taking picture: $e');
-      return null;
-    }
-  }
-
-  Future initCamera(CameraDescription cameraDescription) async {
-    _cameraController =
-        CameraController(cameraDescription, ResolutionPreset.high);
-    try {
-      await _cameraController.initialize().then((_) {
-        if (!mounted) return;
-        setState(() {});
-      });
-    } on CameraException catch (e) {
-      debugPrint("camera error $e");
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Stack(children: [
-            (_cameraController.value.isInitialized)
-                ? CameraPreview(_cameraController)
-                : Container(
-                color: Colors.black,
-                child: const Center(child: CircularProgressIndicator())),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.20,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
-                      color: Colors.black),
-                  child:
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Expanded(
-                        child: IconButton(
-                          padding: EdgeInsets.zero,
-                          iconSize: 30,
-                          icon: Icon(
-                              _isRearCameraSelected
-                                  ? CupertinoIcons.switch_camera
-                                  : CupertinoIcons.switch_camera_solid,
-                              color: Colors.white),
-                          onPressed: () {
-                            setState(
-                                    () => _isRearCameraSelected = !_isRearCameraSelected);
-                            initCamera(widget.cameras![_isRearCameraSelected ? 0 : 1]);
-                          },
-                        )),
-                    Expanded(
-                        child: IconButton(
-                          onPressed: takePicture,
-                          iconSize: 50,
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          icon: const Icon(Icons.circle, color: Colors.white),
-                        )),
-                    const Spacer(),
-                  ]),
-                )),
-          ]),
-        ));
+      appBar: AppBar(
+        title: Text('Camera Page'),
+      ),
+      body: _cameraController.value.isInitialized
+          ? CameraPreview(_cameraController)
+          : CircularProgressIndicator(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: Icon(Icons.camera),
+      ),
+    );
   }
 }
-
-
--- smartfarm/lib/pages/deteksi/preview.dart
+++ smartfarm/lib/pages/deteksi/preview.dart
@@ -2,10 +2,13 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:smartfarm/model/response_model.dart';
+
 class PreviewPage extends StatelessWidget {
-  const PreviewPage({Key? key, required this.picture}) : super(key: key);
+  const PreviewPage({Key? key, required this.picture, required this.apiResponse}) : super(key: key);
 
   final XFile picture;
+  final ApiResponse apiResponse; // Change the type here
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +19,13 @@ class PreviewPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               IconButton(
-                onPressed: (){
+                onPressed: () {
                   Navigator.pop(context);
                 },
                 icon: Icon(
                   Icons.arrow_back_outlined,
                 ),
-
-              ), //pembuatan tombol akhir
+              ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.22,
               ),
@@ -31,11 +33,21 @@ class PreviewPage extends StatelessWidget {
             ],
           ),
           Center(
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
-              const SizedBox(height: 24),
-              Text(picture.name)
-            ]),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
+                const SizedBox(height: 24),
+                Text(picture.name),
+                SizedBox(height: 16),
+                Text("Prediction: ${apiResponse.prediction}"),
+                Text("Confidence: ${apiResponse.confidence}"),
+                Text("Description: ${apiResponse.description}"),
+                Text("Pengendalian Hayati: ${apiResponse.pengendalianHayati}"),
+                Text("Pengendalian Kimiawi: ${apiResponse.pengendalianKimiawi}"),
+                // Tambahkan widget lain sesuai kebutuhan
+              ],
+            ),
           ),
         ],
       ),
